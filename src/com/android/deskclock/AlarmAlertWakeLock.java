5aee14904f3b7272b5a1741fe820a8ed73b96664
Robyn Coultas <coultasr@android.com>
2012-10-07 14:24:22 -0700

    Wake screen when timer klaxon starts
    Bug:7300009
    Change-Id: Id84cf53c373bca19a3086f9c522ee3ea8d729760

--- src/com/android/deskclock/AlarmAlertWakeLock.java
+++ src/com/android/deskclock/AlarmAlertWakeLock.java
@@ -42,6 +42,16 @@ class AlarmAlertWakeLock {
         sCpuWakeLock.acquire();
     }
 
+    static void acquireScreenCpuWakeLock(Context context) {
+        if (sCpuWakeLock != null) {
+            return;
+        }
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        sCpuWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK
+                | PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, Log.LOGTAG);
+        sCpuWakeLock.acquire();
+    }
+
     static void releaseCpuLock() {
         if (sCpuWakeLock != null) {
             sCpuWakeLock.release();
