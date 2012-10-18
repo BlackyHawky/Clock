78b8e1513e24c58ffea6ee4edbebdce85c248f6f
Sam Blitzstein <sblitz@google.com>
2012-10-17 18:31:27 -0700

    Replace lights out mode with screensaver.
    Removed the lights out code.
    Added in explicit screensaver mode.
    Also fixed bug related to timer labels on device rotation.
    
    Bug: 7328141
    Bug: 7339168
    Change-Id: I7df3567888ee04ad9a335c34d51384bfaf24e6b3

--- src/com/android/deskclock/Utils.java
+++ src/com/android/deskclock/Utils.java
@@ -16,19 +16,36 @@
 
 package com.android.deskclock;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.TimeInterpolator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.net.Uri;
+import android.os.Handler;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
 
 import com.android.deskclock.stopwatch.Stopwatches;
 import com.android.deskclock.timer.Timers;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 
@@ -47,7 +64,15 @@ public class Utils {
      */
     private static String sCachedVersionCode = null;
 
+    /**
+     * Intent to be used for checking if a clock's date has changed. Must be every fifteen
+     * minutes because not all time zones are hour-locked.
+     **/
+    public static final String ACTION_ON_QUARTER_HOUR = "com.android.deskclock.ON_QUARTER_HOUR";
 
+    /** Types that may be used for clock displays. **/
+    public static final String CLOCK_TYPE_DIGITAL = "digital";
+    public static final String CLOCK_TYPE_ANALOG = "analog";
 
     public static void prepareHelpMenuItem(Context context, MenuItem helpMenuItem) {
         String helpUrlString = context.getResources().getString(R.string.desk_clock_help_url);
@@ -159,4 +184,217 @@ public class Utils {
         timerIntent.setAction(Timers.NOTIF_IN_USE_SHOW);
         context.sendBroadcast(timerIntent);
     }
+
+    /** Runnable for use with screensaver and dream, to move the clock every minute.
+     *  registerViews() must be called prior to posting.
+     */
+    public static class ScreensaverMoveSaverRunnable implements Runnable {
+        static final long MOVE_DELAY = 60000; // DeskClock.SCREEN_SAVER_MOVE_DELAY;
+        static final long SLIDE_TIME = 10000;
+        static final long FADE_TIME = 3000;
+
+        static final boolean SLIDE = false;
+
+        private View mContentView, mSaverView;
+        private final Handler mHandler;
+
+        private static TimeInterpolator mSlowStartWithBrakes;
+
+
+        public ScreensaverMoveSaverRunnable(Handler handler) {
+            mHandler = handler;
+            mSlowStartWithBrakes = new TimeInterpolator() {
+                @Override
+                public float getInterpolation(float x) {
+                    return (float)(Math.cos((Math.pow(x,3) + 1) * Math.PI) / 2.0f) + 0.5f;
+                }
+            };
+        }
+
+        public void registerViews(View contentView, View saverView) {
+            mContentView = contentView;
+            mSaverView = saverView;
+        }
+
+        @Override
+        public void run() {
+            long delay = MOVE_DELAY;
+            if (mContentView == null || mSaverView == null) {
+                mHandler.removeCallbacks(this);
+                mHandler.postDelayed(this, delay);
+                return;
+            }
+
+            final float xrange = mContentView.getWidth() - mSaverView.getWidth();
+            final float yrange = mContentView.getHeight() - mSaverView.getHeight();
+            Log.v("xrange: "+xrange+" yrange: "+yrange);
+
+            if (xrange == 0 && yrange == 0) {
+                delay = 500; // back in a split second
+            } else {
+                final int nextx = (int) (Math.random() * xrange);
+                final int nexty = (int) (Math.random() * yrange);
+
+                if (mSaverView.getAlpha() == 0f) {
+                    // jump right there
+                    mSaverView.setX(nextx);
+                    mSaverView.setY(nexty);
+                    ObjectAnimator.ofFloat(mSaverView, "alpha", 0f, 1f)
+                        .setDuration(FADE_TIME)
+                        .start();
+                } else {
+                    AnimatorSet s = new AnimatorSet();
+                    Animator xMove   = ObjectAnimator.ofFloat(mSaverView,
+                                         "x", mSaverView.getX(), nextx);
+                    Animator yMove   = ObjectAnimator.ofFloat(mSaverView,
+                                         "y", mSaverView.getY(), nexty);
+
+                    Animator xShrink = ObjectAnimator.ofFloat(mSaverView, "scaleX", 1f, 0.85f);
+                    Animator xGrow   = ObjectAnimator.ofFloat(mSaverView, "scaleX", 0.85f, 1f);
+
+                    Animator yShrink = ObjectAnimator.ofFloat(mSaverView, "scaleY", 1f, 0.85f);
+                    Animator yGrow   = ObjectAnimator.ofFloat(mSaverView, "scaleY", 0.85f, 1f);
+                    AnimatorSet shrink = new AnimatorSet(); shrink.play(xShrink).with(yShrink);
+                    AnimatorSet grow = new AnimatorSet(); grow.play(xGrow).with(yGrow);
+
+                    Animator fadeout = ObjectAnimator.ofFloat(mSaverView, "alpha", 1f, 0f);
+                    Animator fadein = ObjectAnimator.ofFloat(mSaverView, "alpha", 0f, 1f);
+
+
+                    if (SLIDE) {
+                        s.play(xMove).with(yMove);
+                        s.setDuration(SLIDE_TIME);
+
+                        s.play(shrink.setDuration(SLIDE_TIME/2));
+                        s.play(grow.setDuration(SLIDE_TIME/2)).after(shrink);
+                        s.setInterpolator(mSlowStartWithBrakes);
+                    } else {
+                        AccelerateInterpolator accel = new AccelerateInterpolator();
+                        DecelerateInterpolator decel = new DecelerateInterpolator();
+
+                        shrink.setDuration(FADE_TIME).setInterpolator(accel);
+                        fadeout.setDuration(FADE_TIME).setInterpolator(accel);
+                        grow.setDuration(FADE_TIME).setInterpolator(decel);
+                        fadein.setDuration(FADE_TIME).setInterpolator(decel);
+                        s.play(shrink);
+                        s.play(fadeout);
+                        s.play(xMove.setDuration(0)).after(FADE_TIME);
+                        s.play(yMove.setDuration(0)).after(FADE_TIME);
+                        s.play(fadein).after(FADE_TIME);
+                        s.play(grow).after(FADE_TIME);
+                    }
+                    s.start();
+                }
+
+                long now = System.currentTimeMillis();
+                long adjust = (now % 60000);
+                delay = delay
+                        + (MOVE_DELAY - adjust) // minute aligned
+                        - (SLIDE ? 0 : FADE_TIME) // start moving before the fade
+                        ;
+            }
+
+            mHandler.removeCallbacks(this);
+            mHandler.postDelayed(this, delay);
+        }
+    }
+
+    /** Setup to find out when the quarter-hour changes (e.g. Kathmandu is GMT+5:45) **/
+    public static long getAlarmOnQuarterHour() {
+        Calendar nextQuarter = Calendar.getInstance();
+        //  Set 1 second to ensure quarter-hour threshold passed.
+        nextQuarter.set(Calendar.SECOND, 1);
+        int minute = nextQuarter.get(Calendar.MINUTE);
+        nextQuarter.add(Calendar.MINUTE, 15 - (minute % 15));
+        long alarmOnQuarterHour = nextQuarter.getTimeInMillis();
+        if (0 >= (alarmOnQuarterHour - System.currentTimeMillis())
+                || (alarmOnQuarterHour - System.currentTimeMillis()) > 901000) {
+            Log.wtf("quarterly alarm calculation error");
+        }
+        return alarmOnQuarterHour;
+    }
+
+    /**
+     * For screensavers to set whether the digital or analog clock should be displayed.
+     * Returns the view to be displayed.
+     */
+    public static View setClockStyle(Context context, View digitalClock, View analogClock,
+            String clockStyleKey) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        String defaultClockStyle = context.getResources().getString(R.string.default_clock_style);
+        String style = sharedPref.getString(clockStyleKey, defaultClockStyle);
+        View returnView;
+        if (style.equals(CLOCK_TYPE_ANALOG)) {
+            digitalClock.setVisibility(View.GONE);
+            analogClock.setVisibility(View.VISIBLE);
+            returnView = analogClock;
+        } else {
+            digitalClock.setVisibility(View.VISIBLE);
+            analogClock.setVisibility(View.GONE);
+            returnView = digitalClock;
+        }
+
+        return returnView;
+    }
+
+    /**
+     * For screensavers to dim the lights if necessary.
+     */
+    public static void dimClockView(boolean dim, View clockView) {
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setColorFilter(new PorterDuffColorFilter(
+                        (dim ? 0x60FFFFFF : 0xC0FFFFFF),
+                PorterDuff.Mode.MULTIPLY));
+        clockView.setLayerType(View.LAYER_TYPE_HARDWARE, paint);
+    }
+
+    /** Clock views can call this to refresh their alarm to the next upcoming value. **/
+    public static void refreshAlarm(
+            Context context, String clockStyle, View analogClock, View digitalClock) {
+        String nextAlarm = Settings.System.getString(context.getContentResolver(),
+                Settings.System.NEXT_ALARM_FORMATTED);
+        TextView nextAlarmView;
+        View slash;
+        Log.v("style: "+clockStyle);
+        if (clockStyle.equals(CLOCK_TYPE_ANALOG)) {
+            nextAlarmView = (TextView) analogClock.findViewById(R.id.nextAlarm);
+            slash = analogClock.findViewById(R.id.slash);
+        } else {
+            nextAlarmView = (TextView) digitalClock.findViewById(R.id.nextAlarm_digital);
+            slash = digitalClock.findViewById(R.id.slash_digital);
+        }
+        if (!TextUtils.isEmpty(nextAlarm) && nextAlarmView != null) {
+            nextAlarmView.setText(
+                    context.getString(R.string.control_set_alarm_with_existing, nextAlarm));
+            nextAlarmView.setContentDescription(context.getResources().getString(
+                    R.string.next_alarm_description, nextAlarm));
+            nextAlarmView.setVisibility(View.VISIBLE);
+            slash.setVisibility(View.VISIBLE);
+        } else  {
+            nextAlarmView.setVisibility(View.GONE);
+            slash.setVisibility(View.GONE);
+        }
+    }
+
+    /** Clock views can call this to refresh their date. **/
+    public static void updateDate(String dateFormat, String dateFormatForAccessibility,
+            String clockStyle, View analogClock, View digitalClock) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(System.currentTimeMillis());
+
+        CharSequence newDate = DateFormat.format(dateFormat, cal);
+        TextView dateDisplay;
+        if (clockStyle.equals(CLOCK_TYPE_ANALOG)) {
+            dateDisplay = (TextView) analogClock.findViewById(R.id.date);
+        } else {
+            dateDisplay = (TextView) digitalClock.findViewById(R.id.date_digital);
+        }
+        if (dateDisplay != null) {
+            dateDisplay.setVisibility(View.VISIBLE);
+            dateDisplay.setText(newDate);
+            dateDisplay.setContentDescription(DateFormat.format(dateFormatForAccessibility, cal));
+        }
+    }
+
 }
