4c51096686fb188fddbe85386fdbabf3ebe4dbd0
Paul Sliwowski <psliwowski@google.com>
2013-08-15 18:24:32 -0700

    Remove dead activity.
    Change-Id: If94d399470d69f51fad4e968e4473f305343faa9

--- src/com/android/deskclock/AlarmUtils.java
+++ src/com/android/deskclock/AlarmUtils.java
@@ -26,7 +26,6 @@ import android.widget.Toast;
  * Static utility methods for Alarms.
  */
 public class AlarmUtils {
-
     public static void showTimeEditDialog(FragmentManager manager, final Alarm alarm) {
         final FragmentTransaction ft = manager.beginTransaction();
         final Fragment prev = manager.findFragmentByTag("time_dialog");
@@ -39,8 +38,43 @@ public class AlarmUtils {
         fragment.show(ft, "time_dialog");
     }
 
+    /**
+     * format "Alarm set for 2 days 7 hours and 53 minutes from
+     * now"
+     */
+    private static String formatToast(Context context, long timeInMillis) {
+        long delta = timeInMillis - System.currentTimeMillis();
+        long hours = delta / (1000 * 60 * 60);
+        long minutes = delta / (1000 * 60) % 60;
+        long days = hours / 24;
+        hours = hours % 24;
+
+        String daySeq = (days == 0) ? "" :
+                (days == 1) ? context.getString(R.string.day) :
+                        context.getString(R.string.days, Long.toString(days));
+
+        String minSeq = (minutes == 0) ? "" :
+                (minutes == 1) ? context.getString(R.string.minute) :
+                        context.getString(R.string.minutes, Long.toString(minutes));
+
+        String hourSeq = (hours == 0) ? "" :
+                (hours == 1) ? context.getString(R.string.hour) :
+                        context.getString(R.string.hours, Long.toString(hours));
+
+        boolean dispDays = days > 0;
+        boolean dispHour = hours > 0;
+        boolean dispMinute = minutes > 0;
+
+        int index = (dispDays ? 1 : 0) |
+                (dispHour ? 2 : 0) |
+                (dispMinute ? 4 : 0);
+
+        String[] formats = context.getResources().getStringArray(R.array.alarm_set);
+        return String.format(formats[index], daySeq, hourSeq, minSeq);
+    }
+
     public static void popAlarmSetToast(Context context, long timeInMillis) {
-        String toastText = SetAlarm.formatToast(context, timeInMillis);
+        String toastText = formatToast(context, timeInMillis);
         Toast toast = Toast.makeText(context, toastText, Toast.LENGTH_LONG);
         ToastMaster.setToast(toast);
         toast.show();
