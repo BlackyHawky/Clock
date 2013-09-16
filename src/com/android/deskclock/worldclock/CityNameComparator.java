d9f3456cf8a1e44704e2a4e2cf921e4b0600802f
James Kung <kingkung@google.com>
2013-09-15 21:21:37 -0700

    Storing collator in memory for CityNameComparator
    Bug: 10750201
    Change-Id: I8dd82c47225a711c97274ae8508c0f061b02f784
    (cherry picked from commit d24d9e3c7cd03cd03dc87002ee5af5fee2bfa0fb)

--- src/com/android/deskclock/worldclock/CityNameComparator.java
+++ src/com/android/deskclock/worldclock/CityNameComparator.java
@@ -20,8 +20,14 @@ import java.util.Comparator;
 
 public class CityNameComparator implements Comparator<CityObj> {
 
+    private Collator mCollator;
+
+    public CityNameComparator() {
+        mCollator = Collator.getInstance();
+    }
+
     @Override
     public int compare(CityObj c1, CityObj c2) {
-        return Collator.getInstance().compare(c1.mCityName, c2.mCityName);
+        return mCollator.compare(c1.mCityName, c2.mCityName);
     }
 }
