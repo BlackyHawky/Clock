d968d6707f61078c3afacffa08c499ee9ed472a5
Alan Viverette <alanv@google.com>
2013-09-25 17:53:05 -0700

    Use drag-to-open overflow menu in Alarm and Clock fragments
    BUG: 10903795
    Change-Id: I6288b5d4d579cc7a0aa8c0142d3249c3da232b3e

--- src/com/android/deskclock/DeskClockFragment.java
+++ src/com/android/deskclock/DeskClockFragment.java
@@ -17,10 +17,44 @@
 package com.android.deskclock;
 
 import android.app.Fragment;
+import android.support.v4.widget.PopupMenuCompat;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class DeskClockFragment extends Fragment {
 
     public void onPageChanged(int page) {
         // Do nothing here , only in derived classes
+    }
+
+    /**
+     * Installs click and touch listeners on a fake overflow menu button.
+     *
+     * @param menuButton the fragment's fake overflow menu button
+     */
+    public void setupFakeOverflowMenuButton(View menuButton) {
+        final PopupMenu fakeOverflow = new PopupMenu(menuButton.getContext(), menuButton) {
+            @Override
+            public void show() {
+                getActivity().onPrepareOptionsMenu(getMenu());
+                super.show();
+            }
+        };
+        fakeOverflow.inflate(R.menu.desk_clock_menu);
+        fakeOverflow.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener () {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return getActivity().onOptionsItemSelected(item);
+            }
+        });
+
+        menuButton.setOnTouchListener(PopupMenuCompat.getDragToOpenListener(fakeOverflow));
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fakeOverflow.show();
+            }
+        });
     }
 }
