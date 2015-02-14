d5545aca39f5eb39fb9795324fd09a19ae11efba
Annie Chin <afchin@google.com>
2015-02-13 17:02:26 -0800

    Fix build breakage
    Change-Id: I452adbf71628dfa6a32f138b7da5cefdcafeede1

--- Android.mk
+++ Android.mk
@@ -1,10 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-datetimepicker_dir := ../../../../frameworks/opt/datetimepicker/res
-res_dirs := $(datetimepicker_dir)
 LOCAL_RESOURCE_DIR := packages/apps/DeskClock/res
-LOCAL_RESOURCE_DIR += $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+LOCAL_RESOURCE_DIR += frameworks/opt/datetimepicker/res
+LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/appcompat/res
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := current
@@ -17,11 +16,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
 LOCAL_STATIC_JAVA_LIBRARIES += android-opt-datetimepicker
-
-APPCOMPAT_DIR := prebuilts/sdk/current/support/v7/appcompat
-
-LOCAL_RESOURCE_DIR := packages/apps/DeskClock/res
-LOCAL_RESOURCE_DIR += $(APPCOMPAT_DIR)/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
