639b7025aaa1fe5417ee01e81ec7598c94ba86d8
Justin Klaassen <justinklaassen@google.com>
2015-02-23 16:37:10 -0800

    Add dependency on android.support.v7.gridlayout
    Bug: 19486309
    Change-Id: I47484e90ba0607dd50bc6ca65c658b0eafe1e952

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_RESOURCE_DIR := packages/apps/DeskClock/res
 LOCAL_RESOURCE_DIR += frameworks/opt/datetimepicker/res
 LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/appcompat/res
+LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/gridlayout/res
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := current
@@ -13,12 +14,14 @@ LOCAL_OVERRIDES_PACKAGES := AlarmClock
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+LOCAL_STATIC_JAVA_LIBRARIES := android-opt-datetimepicker
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
-LOCAL_STATIC_JAVA_LIBRARIES += android-opt-datetimepicker
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-gridlayout
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.gridlayout
 LOCAL_AAPT_FLAGS += --extra-packages com.android.datetimepicker
 
 include $(BUILD_PACKAGE)
