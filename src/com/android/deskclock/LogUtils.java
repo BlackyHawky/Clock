bfd290361106e4f8f91a48c3b01b7bd21971ec1d
Annie Chin <afchin@google.com>
2015-04-02 10:45:46 -0700

    Add check for args.length == 0 in LogUtils.
    Bug: 19972441
    Change-Id: I5de2e159ed87aa19c6a854e741ad4faa99e10e3a

--- src/com/android/deskclock/LogUtils.java
+++ src/com/android/deskclock/LogUtils.java
@@ -30,61 +30,71 @@ public class LogUtils {
 
     public static void v(String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.VERBOSE)) {
-            Log.v(LOGTAG, args == null ? message : String.format(message, args));
+            Log.v(LOGTAG, args == null || args.length == 0
+                    ? message : String.format(message, args));
         }
     }
 
     public static void v(String tag, String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.VERBOSE)) {
-            Log.v(LOGTAG + "/" + tag, args == null ? message : String.format(message, args));
+            Log.v(LOGTAG + "/" + tag, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void d(String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.DEBUG)) {
-            Log.d(LOGTAG, args == null ? message : String.format(message, args));
+            Log.d(LOGTAG, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void d(String tag, String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.DEBUG)) {
-            Log.d(LOGTAG + "/" + tag, args == null ? message : String.format(message, args));
+            Log.d(LOGTAG + "/" + tag, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void i(String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.INFO)) {
-            Log.i(LOGTAG, args == null ? message : String.format(message, args));
+            Log.i(LOGTAG, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void i(String tag, String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.INFO)) {
-            Log.i(LOGTAG + "/" + tag, args == null ? message : String.format(message, args));
+            Log.i(LOGTAG + "/" + tag, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void w(String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.WARN)) {
-            Log.w(LOGTAG, args == null ? message : String.format(message, args));
+            Log.w(LOGTAG, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void w(String tag, String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.WARN)) {
-            Log.w(LOGTAG + "/" + tag, args == null ? message : String.format(message, args));
+            Log.w(LOGTAG + "/" + tag, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void e(String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.ERROR)) {
-            Log.e(LOGTAG, args == null ? message : String.format(message, args));
+            Log.e(LOGTAG, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void e(String tag, String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.ERROR)) {
-            Log.e(LOGTAG + "/" + tag, args == null ? message : String.format(message, args));
+            Log.e(LOGTAG + "/" + tag, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
@@ -102,13 +112,15 @@ public class LogUtils {
 
     public static void wtf(String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.ASSERT)) {
-            Log.wtf(LOGTAG, args == null ? message : String.format(message, args));
+            Log.wtf(LOGTAG, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 
     public static void wtf(String tag, String message, Object... args) {
         if (DEBUG || Log.isLoggable(LOGTAG, Log.ASSERT)) {
-            Log.wtf(LOGTAG + "/" + tag, args == null ? message : String.format(message, args));
+            Log.wtf(LOGTAG + "/" + tag, args == null || args.length == 0 ? message
+                    : String.format(message, args));
         }
     }
 }
