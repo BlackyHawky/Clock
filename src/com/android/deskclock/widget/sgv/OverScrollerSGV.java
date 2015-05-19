9995737da8e1bf899781fbac5a47f7157f78fde3
Daria Evdokimova <dariaevdo@google.com>
2015-05-18 17:52:48 -0700

    DO NOT MERGE Mirroring merge conflict resolution from mnc-dev
    ag/692970
    
    Change-Id: I5155777a29a6c6dd08bd7d9c9d0f15857642670e

--- src/com/android/deskclock/widget/sgv/OverScrollerSGV.java
+++ src/com/android/deskclock/widget/sgv/OverScrollerSGV.java
@@ -176,9 +176,7 @@ public class OverScrollerSGV {
      * @return The original velocity less the deceleration, norm of the X and Y velocity vector.
      */
     public float getCurrVelocity() {
-        float squaredNorm = mScrollerX.mCurrVelocity * mScrollerX.mCurrVelocity;
-        squaredNorm += mScrollerY.mCurrVelocity * mScrollerY.mCurrVelocity;
-        return (float) Math.sqrt(squaredNorm);
+        return (float) Math.hypot(mScrollerX.mCurrVelocity, mScrollerY.mCurrVelocity);
     }
 
     /**
