b4d942af3e1140db824ad2ceee735f5d75809baf
Annie Chin <afchin@google.com>
2015-06-04 10:26:40 -0700

    Make stopwatch rows unclickable.
    Bug: 21620890
    Change-Id: Id5e6905af1706d7ba9cd7ee1a97192e86b731893

--- src/com/android/deskclock/stopwatch/StopwatchFragment.java
+++ src/com/android/deskclock/stopwatch/StopwatchFragment.java
@@ -130,6 +130,11 @@ public class StopwatchFragment extends DeskClockFragment
         }
 
         @Override
+        public boolean isEnabled(int position) {
+            return false;
+        }
+
+        @Override
         public long getItemId(int position) {
             return position;
         }
