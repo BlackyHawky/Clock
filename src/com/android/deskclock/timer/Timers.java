5ae0b399eaa79fc9889086a7a0dc039a666d5265
Annie Chin <afchin@google.com>
2015-07-10 13:09:04 -0700

    Scroll to the started timer when timer is started through API call.
    Bug: 22224832
    
    Previously defaulted to the first timer in the list due to the
    assumption that the started timer would be new.
    
    Change-Id: Ibb42b14e3d47f7e9ded9460722a509c75ab9cb1a

--- src/com/android/deskclock/timer/Timers.java
+++ src/com/android/deskclock/timer/Timers.java
@@ -46,6 +46,7 @@ public class Timers {
     public static final String NOTIF_TIMES_UP_SHOW = "notif_times_up_show";
     public static final String NOTIF_TIMES_UP_CANCEL = "notif_times_up_cancel";
     public static final String FIRST_LAUNCH_FROM_API_CALL = "first_launch_from_api_call";
+    public static final String SCROLL_TO_TIMER_ID = "scroll_to_timer_id";
 
     public static final String TIMESUP_MODE = "times_up";
 
