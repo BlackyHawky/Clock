2b84c7a58d6f23a604317c6b2a1e586e6763fcea
Fan Zhang <zhfan@google.com>
2015-08-19 17:33:10 -0700

    Set alarm item content description to alarm time.
    Bug: 23376863
    
    Change-Id: I53f2c6696d14e72d336c4c3cfea5c5726821379f

--- src/com/android/deskclock/alarms/dataadapter/CollapsedAlarmViewHolder.java
+++ src/com/android/deskclock/alarms/dataadapter/CollapsedAlarmViewHolder.java
@@ -17,6 +17,7 @@
 package com.android.deskclock.alarms.dataadapter;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
@@ -87,6 +88,7 @@ public final class CollapsedAlarmViewHolder extends AlarmTimeViewHolder {
         boolean boundPreemptiveDismiss =
                 bindPreemptiveDismissButton(context, alarm, alarmInstance);
         hairLine.setVisibility(boundPreemptiveDismiss ? View.GONE : View.VISIBLE);
+        itemView.setContentDescription(clock.getContentDescription());
     }
 
     private void bindReadOnlyLabel(Context context, Alarm alarm) {
@@ -103,7 +105,7 @@ public final class CollapsedAlarmViewHolder extends AlarmTimeViewHolder {
     private void bindRepeatText(Context context, Alarm alarm) {
         final String daysOfWeekText =
                 alarm.daysOfWeek.toString(context, Utils.getFirstDayOfWeek(context));
-        if (daysOfWeek != null && daysOfWeek.length() != 0) {
+        if (!TextUtils.isEmpty(daysOfWeekText)) {
             daysOfWeek.setText(daysOfWeekText);
             daysOfWeek.setContentDescription(alarm.daysOfWeek.toAccessibilityString(
                     context, Utils.getFirstDayOfWeek(context)));
