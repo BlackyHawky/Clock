d12660b6a2f49edbbcb80fdbac0e594148b151af
Justin Klaassen <justinklaassen@google.com>
2016-03-21 18:15:01 -0700

    Use AlphaSafeImageButton subclass - DO NOT MERGE
    Bug: 27715789
    
    On some platforms animating a View's alpha with a RippleDrawable
    background can cause graphical artifacts. To combat this we provide a
    lightweight ImageButton subclass which returns false for
    View#hasOverlappingRendering() if it has a RippleDrawable for a
    background. Fixed in v24.0.0 version of appcompat support lib.
    
    Change-Id: I1baf16c11e9adfe6cbbd4b080aa7fa93915d888f

---
+++ src/com/android/deskclock/widget/AlphaSafeImageButton.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.deskclock.widget;
+
+import android.content.Context;
+import android.graphics.drawable.RippleDrawable;
+import android.support.v7.widget.AppCompatImageButton;
+import android.util.AttributeSet;
+
+/**
+ * A lightweight {@code ImageButton} subclass to provide a workaround for bad interactions
+ * when animating a View's alpha with a {@link RippleDrawable} background.
+ */
+public class AlphaSafeImageButton extends AppCompatImageButton {
+
+    public AlphaSafeImageButton(Context context) {
+        super(context);
+    }
+
+    public AlphaSafeImageButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AlphaSafeImageButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * Returns {@code false} if the background is a {@link RippleDrawable}.
+     */
+    @Override
+    public boolean hasOverlappingRendering() {
+        return !(getBackground() instanceof RippleDrawable) && super.hasOverlappingRendering();
+    }
+}
