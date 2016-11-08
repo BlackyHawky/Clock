0b19985ecfa0f43acb8ededc173f643843098a95
James Lemieux <jplemieux@google.com>
2016-11-08 15:54:42 -0800

    Stop misrepresenting alarm time on DST days
    Bug: 27654884
    
    The alarms list displays the alarm time by formatting a Calendar object.
    The Calendar object is always created on the current date. If the
    current date happens to be the day DST is applied and the alarm time
    falls in the missing hour (in the spring), Calendar adjusts the hour ahead
    an hour, causing the alarm time to be incorrectly displayed on screen.
    
    The Calendar object is now created on a fixed non-DST day to avoid
    applying DST rules inappropriately.
    
    Change-Id: Ibf3f86a84171edcf819a132fc3f3efdd4f32dd5d

--- src/com/android/deskclock/data/TimeModel.java
+++ src/com/android/deskclock/data/TimeModel.java
@@ -16,13 +16,23 @@
 
 package com.android.deskclock.data;
 
+import android.content.Context;
 import android.os.SystemClock;
+import android.text.format.DateFormat;
+
+import java.util.Calendar;
 
 /**
  * All time data is accessed via this model. This model exists so that time can be mocked for
  * testing purposes.
  */
 final class TimeModel {
+
+    private final Context mContext;
+
+    TimeModel(Context context) {
+        mContext = context;
+    }
 
     /**
      * @return the current time in milliseconds
@@ -36,5 +46,21 @@ final class TimeModel {
      */
     long elapsedRealtime() {
         return SystemClock.elapsedRealtime();
+    }
+
+    /**
+     * @return {@code true} if 24 hour time format is selected; {@code false} otherwise
+     */
+    boolean is24HourFormat() {
+        return DateFormat.is24HourFormat(mContext);
+    }
+
+    /**
+     * @return a new Calendar with the {@link #currentTimeMillis}
+     */
+    Calendar getCalendar() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(currentTimeMillis());
+        return calendar;
     }
 }
