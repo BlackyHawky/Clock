bfc5eb5e10be255e354f23870585128ddc5ac4e7
Alan Viverette <alanv@google.com>
2017-04-21 16:15:46 -0400

    Update to use STATIC_ANDROID_LIBRARIES
    Test: make DeskClock
    Change-Id: I7b8cd03ed3fa3d457bade37c5b049d22b154248e

--- Android.mk
+++ Android.mk
@@ -3,15 +3,6 @@ include $(CLEAR_VARS)
 
 LOCAL_RESOURCE_DIR := packages/apps/DeskClock/res
 
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/design/res
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/percent/res
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/transition/res
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v14/preference/res
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/appcompat/res
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/gridlayout/res
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/preference/res
-LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/recyclerview/res
-
 LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := current
 
@@ -24,25 +15,17 @@ LOCAL_PROGUARD_FLAG_FILES := ../../../frameworks/support/design/proguard-rules.p
 LOCAL_PROGUARD_FLAG_FILES += ../../../frameworks/support/v7/preference/proguard-rules.pro
 LOCAL_PROGUARD_FLAG_FILES += ../../../frameworks/support/v7/recyclerview/proguard-rules.pro
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-design-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-percent-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-transition-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v14-preference-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-gridlayout-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-preference-prebuilt
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview-prebuilt
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+        android-support-design \
+        android-support-percent \
+        android-support-transition \
+        android-support-v13 \
+        android-support-v14-preference \
+        android-support-v7-appcompat \
+        android-support-v7-gridlayout \
+        android-support-v7-preference \
+        android-support-v7-recyclerview
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages android.support.compat
-LOCAL_AAPT_FLAGS += --extra-packages android.support.design
-LOCAL_AAPT_FLAGS += --extra-packages android.support.percent
-LOCAL_AAPT_FLAGS += --extra-packages android.support.transition
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v14.preference
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.gridlayout
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.preference
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
+LOCAL_USE_AAPT2 := true
 
 include $(BUILD_PACKAGE)
