cd7108e77ae0b2d22250bfb734ce6669f5102b2d
LuK1337 <priv.luk@gmail.com>
2020-07-14 20:07:19 +0200

    DeskClock: Setup data model before calling super.onStart()
    * Otherwise onStart() in StopwatchFragment will be called
      when app still believes it's running in the background and
      thus will remove FLAG_KEEP_SCREEN_ON window flag.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/2258
    Change-Id: I196051d0d03f479c7ee3d0f7735cf8e64b70d70d

--- src/com/android/deskclock/DeskClock.java
+++ src/com/android/deskclock/DeskClock.java
@@ -303,9 +303,9 @@ public class DeskClock extends BaseActivity
 
     @Override
     protected void onStart() {
-        super.onStart();
         DataModel.getDataModel().addSilentSettingsListener(mSilentSettingChangeWatcher);
         DataModel.getDataModel().setApplicationInForeground(true);
+        super.onStart();
     }
 
     @Override
