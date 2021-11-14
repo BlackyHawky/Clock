b135b9a4c5dfc88e8479566231856e0a9ba5c5f5
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 21:11:09 +0100

    Update Utils.java

--- src/com/android/deskclock/Utils.java
+++ src/com/android/deskclock/Utils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.deskclock;
+package com.best.deskclock;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
