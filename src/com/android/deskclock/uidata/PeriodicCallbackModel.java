9e34fcdaca4f891889a176c7ce937373e4bfe2fa
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 19:40:57 +0100

    Update PeriodicCallbackModel.java

--- src/com/android/deskclock/uidata/PeriodicCallbackModel.java
+++ src/com/android/deskclock/uidata/PeriodicCallbackModel.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.deskclock.uidata;
+package com.best.deskclock.uidata;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
