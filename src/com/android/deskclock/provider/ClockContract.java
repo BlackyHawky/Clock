7772257984f0fa711302b6ca7e9bc5d824248508
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 19:17:19 +0100

    Update ClockContract.java

--- src/com/android/deskclock/provider/ClockContract.java
+++ src/com/android/deskclock/provider/ClockContract.java
@@ -13,12 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.deskclock.provider;
+package com.best.deskclock.provider;
 
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-import com.android.deskclock.BuildConfig;
+import com.best.deskclock.BuildConfig;
 
 /**
  * <p>
