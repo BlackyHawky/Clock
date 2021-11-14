84fa3b88db0fc0ba2894e0afbe69baab0838063d
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 19:13:36 +0100

    Update LogEventTracker.java

--- src/com/android/deskclock/events/LogEventTracker.java
+++ src/com/android/deskclock/events/LogEventTracker.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.deskclock.events;
+package com.best.deskclock.events;
 
 import android.content.Context;
 import androidx.annotation.StringRes;
