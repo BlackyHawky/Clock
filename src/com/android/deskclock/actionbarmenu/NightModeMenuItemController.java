6206dae75347384a4bce6a24356773d9ae6d3cf6
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 18:42:11 +0100

    Update NightModeMenuItemController.java

--- src/com/android/deskclock/actionbarmenu/NightModeMenuItemController.java
+++ src/com/android/deskclock/actionbarmenu/NightModeMenuItemController.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.deskclock.actionbarmenu;
+package com.best.deskclock.actionbarmenu;
 
 import android.content.Context;
 import android.content.Intent;
