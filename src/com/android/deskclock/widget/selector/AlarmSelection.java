17bd6f199eccb263c012907eee45d699cfee71df
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 19:43:10 +0100

    Update AlarmSelection.java

--- src/com/android/deskclock/widget/selector/AlarmSelection.java
+++ src/com/android/deskclock/widget/selector/AlarmSelection.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.deskclock.widget.selector;
+package com.best.deskclock.widget.selector;
 
-import com.android.deskclock.provider.Alarm;
+import com.best.deskclock.provider.Alarm;
 
 public class AlarmSelection {
     private final String mLabel;
