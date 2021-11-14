665484aafbb191f826d7a84aead2e2cb9c3f45e5
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 18:51:50 +0100

    Update AlarmService.java

--- src/com/android/deskclock/alarms/AlarmService.java
+++ src/com/android/deskclock/alarms/AlarmService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.deskclock.alarms;
+package com.best.deskclock.alarms;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -30,12 +30,12 @@ import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 
-import com.android.deskclock.AlarmAlertWakeLock;
-import com.android.deskclock.LogUtils;
-import com.android.deskclock.R;
-import com.android.deskclock.data.DataModel;
-import com.android.deskclock.events.Events;
-import com.android.deskclock.provider.AlarmInstance;
+import com.best.deskclock.AlarmAlertWakeLock;
+import com.best.deskclock.LogUtils;
+import com.best.deskclock.R;
+import com.best.deskclock.data.DataModel;
+import com.best.deskclock.events.Events;
+import com.best.deskclock.provider.AlarmInstance;
 
 /**
  * This service is in charge of starting/stopping the alarm. It will bring up and manage the
