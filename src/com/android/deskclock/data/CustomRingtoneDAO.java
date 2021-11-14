b6ba40fa9b5c2039b45b1b380ba4220f299f787a
qw123wh <59417474+qw123wh@users.noreply.github.com>
2021-11-14 19:01:39 +0100

    Update CustomRingtoneDAO.java

--- src/com/android/deskclock/data/CustomRingtoneDAO.java
+++ src/com/android/deskclock/data/CustomRingtoneDAO.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.deskclock.data;
+package com.best.deskclock.data;
 
 import android.content.SharedPreferences;
 import android.net.Uri;
