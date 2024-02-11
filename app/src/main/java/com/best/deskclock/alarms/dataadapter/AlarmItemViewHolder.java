9c6eb447de56982faea7f184440971786dc72f78
BlackyHawky <blackyhawky@protonmail.com>
2024-02-11 15:04:05 +0100

    Correct flicker when reducing or extending alarms
    Re-enable ripple effect when time is clicked

--- app/src/main/java/com/best/deskclock/alarms/dataadapter/AlarmItemViewHolder.java
+++ app/src/main/java/com/best/deskclock/alarms/dataadapter/AlarmItemViewHolder.java
@@ -16,11 +16,17 @@
 
 package com.best.deskclock.alarms.dataadapter;
 
+import static android.util.TypedValue.COMPLEX_UNIT_DIP;
+
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.core.graphics.ColorUtils;
 
 import com.best.deskclock.AlarmUtils;
 import com.best.deskclock.ItemAdapter;
@@ -57,27 +63,26 @@ public abstract class AlarmItemViewHolder extends ItemAdapter.ItemViewHolder<Ala
     public AlarmItemViewHolder(View itemView) {
         super(itemView);
 
+        final int color = itemView.getContext().getColor(R.color.md_theme_primary);
+        final int radius = (int) TypedValue.applyDimension(COMPLEX_UNIT_DIP, 12, itemView.getResources().getDisplayMetrics());
+        final GradientDrawable gradientDrawable = new GradientDrawable();
+        gradientDrawable.setCornerRadius(radius);
+        gradientDrawable.setColor(ColorUtils.setAlphaComponent(color, 20));
+        itemView.setBackground(gradientDrawable);
+
         clock = itemView.findViewById(R.id.digital_clock);
         onOff = itemView.findViewById(R.id.onoff);
         arrow = itemView.findViewById(R.id.arrow);
-        preemptiveDismissButton =
-                itemView.findViewById(R.id.preemptive_dismiss_button);
-        preemptiveDismissButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final AlarmInstance alarmInstance = getItemHolder().getAlarmInstance();
-                if (alarmInstance != null) {
-                    getItemHolder().getAlarmTimeClickHandler().dismissAlarmInstance(alarmInstance);
-                }
+
+        preemptiveDismissButton = itemView.findViewById(R.id.preemptive_dismiss_button);
+        preemptiveDismissButton.setOnClickListener(v -> {
+            final AlarmInstance alarmInstance = getItemHolder().getAlarmInstance();
+            if (alarmInstance != null) {
+                getItemHolder().getAlarmTimeClickHandler().dismissAlarmInstance(alarmInstance);
             }
         });
-        onOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                getItemHolder().getAlarmTimeClickHandler().setAlarmEnabled(
-                        getItemHolder().item, checked);
-            }
-        });
+        onOff.setOnCheckedChangeListener((compoundButton, checked) ->
+                getItemHolder().getAlarmTimeClickHandler().setAlarmEnabled(getItemHolder().item, checked));
     }
 
     @Override
@@ -100,8 +105,7 @@ public abstract class AlarmItemViewHolder extends ItemAdapter.ItemViewHolder<Ala
         clock.setAlpha(alarm.enabled ? CLOCK_ENABLED_ALPHA : CLOCK_DISABLED_ALPHA);
     }
 
-    protected boolean bindPreemptiveDismissButton(Context context, Alarm alarm,
-                                                  AlarmInstance alarmInstance) {
+    protected boolean bindPreemptiveDismissButton(Context context, Alarm alarm, AlarmInstance alarmInstance) {
         final boolean canBind = alarm.canPreemptivelyDismiss() && alarmInstance != null;
         if (canBind) {
             preemptiveDismissButton.setVisibility(View.VISIBLE);
