7f054c1ac766a4431e1ac6fe239f95eafbe03593
BlackyHawky <blackyhawky@protonmail.com>
2024-03-11 23:13:58 +0100

    Fix clock style change
    - When returning from settings, the cities list would keep the old style until it's force-refreshed when the next minute passes;
    - Force an immediate change in onResume() by calling the citiesChanged() method;

--- app/src/main/java/com/best/deskclock/data/CityListener.java
+++ app/src/main/java/com/best/deskclock/data/CityListener.java
@@ -16,11 +16,9 @@
 
 package com.best.deskclock.data;
 
-import java.util.List;
-
 /**
  * The interface through which interested parties are notified of changes to the world cities list.
  */
 public interface CityListener {
-    void citiesChanged(List<City> oldCities, List<City> newCities);
+    void citiesChanged();
 }
