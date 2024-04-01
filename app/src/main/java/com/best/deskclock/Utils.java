dfff71b7d95ec71c06cc725aefbc5ffa4cf06765
BlackyHawky <blackyhawky@protonmail.com>
2024-04-01 12:50:30 +0200

    Bold AM/PM

--- app/src/main/java/com/best/deskclock/Utils.java
+++ app/src/main/java/com/best/deskclock/Utils.java
@@ -639,7 +639,7 @@ public class Utils {
         final Spannable sp = new SpannableString(pattern);
         sp.setSpan(new RelativeSizeSpan(amPmRatio), amPmPos, amPmPos + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         sp.setSpan(new StyleSpan(Typeface.NORMAL), amPmPos, amPmPos + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        sp.setSpan(new TypefaceSpan("sans-serif"), amPmPos, amPmPos + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sp.setSpan(new TypefaceSpan("sans-serif-bold"), amPmPos, amPmPos + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         return sp;
     }
