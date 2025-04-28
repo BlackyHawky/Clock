BlackyHawky/Clock

commit 847d92e33abc7675fe5c78963c5006dbc2499af4

Fix creating a timer from the app shortcut

app/src/main/java/com/best/deskclock/timer/TimerFragment.java
@@ -150,8 +150,8 @@ public final class TimerFragment extends DeskClockFragment {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onResume() {
+        super.onResume();
 
         boolean createTimer = false;
         int showTimerId = -1;

commit 8680fc3b8fa3fec81b5e6a12bbb73cd7c7c9d26f

Fix the floating action button in the timer creation view
- Bug introduced with commit 847d92e: it disappeared when changing screen orientation;

app/src/main/java/com/best/deskclock/timer/TimerFragment.java
@@ -173,6 +173,7 @@ public final class TimerFragment extends DeskClockFragment {
 
             if (mTimerSetupState != null) {
                 mCreateTimerView.setState(mTimerSetupState);
+                updateFab(FAB_AND_BUTTONS_IMMEDIATE);
                 mTimerSetupState = null;
             }
         } else {