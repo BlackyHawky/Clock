ba0793acabbd5a08d932c4f7c3c4abdc936f4f7a
BlackyHawky <blackyhawky@protonmail.com>
2025-09-18 09:24:24 +0200

    Enable multiline input (max 2 lines) for labels dialogs

--- app/src/main/java/com/best/deskclock/LabelDialogFragment.java
+++ app/src/main/java/com/best/deskclock/LabelDialogFragment.java
@@ -209,9 +209,11 @@ public class LabelDialogFragment extends DialogFragment {
 
         mEditLabel = view.findViewById(android.R.id.edit);
         mEditLabel.setText(label);
-        mEditLabel.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        mEditLabel.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
         mEditLabel.selectAll();
         mEditLabel.requestFocus();
+        mEditLabel.setMaxLines(2);
+        mEditLabel.setHorizontallyScrolling(false);
         mEditLabel.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 setLabel();
