01fabf48d249da1ed2bdc0872ae9d4279063ee53
BlackyHawky <blackyhawky@protonmail.com>
2026-01-20 12:12:12 +0100

    Improve timer spinner
    - Apply custom font to hour/minute/second titles;
    - Increase the spinner size;

--- app/src/main/java/com/best/deskclock/timer/CustomTimerSpinnerSetupView.java
+++ app/src/main/java/com/best/deskclock/timer/CustomTimerSpinnerSetupView.java
@@ -2,16 +2,25 @@
 
 package com.best.deskclock.timer;
 
+import static androidx.core.util.TypedValueCompat.dpToPx;
+import static com.best.deskclock.DeskClockApplication.getDefaultSharedPreferences;
+
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.best.deskclock.R;
+import com.best.deskclock.data.SettingsDAO;
+import com.best.deskclock.utils.ThemeUtils;
 
 /**
  * Custom component to display a time selection view using spinners used when creating timers.
@@ -28,7 +37,36 @@ public class CustomTimerSpinnerSetupView extends LinearLayout {
     public CustomTimerSpinnerSetupView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
+        SharedPreferences prefs = getDefaultSharedPreferences(context);
+        Typeface typeFace = ThemeUtils.loadFont(SettingsDAO.getGeneralFont(prefs));
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+
         View rootView = inflate(context, R.layout.timer_spinner_setup_view, this);
+
+        int paddingLeft = (int) dpToPx(ThemeUtils.isTablet() ? 120 : 20, displayMetrics);
+
+        int paddingRight = (int) dpToPx(ThemeUtils.isTablet()
+                ? 120
+                : ThemeUtils.isLandscape()
+                ? 90
+                : 20, displayMetrics
+        );
+
+        int paddingBottom = (int) dpToPx(ThemeUtils.isTablet() && ThemeUtils.isLandscape()
+                ? 60
+                : 0, displayMetrics
+        );
+
+        rootView.setPadding(paddingLeft, 0, paddingRight, paddingBottom);
+
+        TextView hourTitle = rootView.findViewById(R.id.hour_title);
+        TextView minuteTitle = rootView.findViewById(R.id.minute_title);
+        TextView secondTitle = rootView.findViewById(R.id.second_title);
+
+        hourTitle.setTypeface(typeFace);
+        minuteTitle.setTypeface(typeFace);
+        secondTitle.setTypeface(typeFace);
+
         mHourPicker = rootView.findViewById(R.id.hour);
         mMinutePicker = rootView.findViewById(R.id.minute);
         mSecondPicker = rootView.findViewById(R.id.second);
@@ -48,6 +86,7 @@ public class CustomTimerSpinnerSetupView extends LinearLayout {
 
         mHourPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
             picker.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
+
             if (mOnValueChangeListener != null) {
                 mOnValueChangeListener.onValueChange();
             }
@@ -55,6 +94,7 @@ public class CustomTimerSpinnerSetupView extends LinearLayout {
 
         mMinutePicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
             picker.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
+
             if (mOnValueChangeListener != null) {
                 mOnValueChangeListener.onValueChange();
             }
@@ -62,6 +102,7 @@ public class CustomTimerSpinnerSetupView extends LinearLayout {
 
         mSecondPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
             picker.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
+
             if (mOnValueChangeListener != null) {
                 mOnValueChangeListener.onValueChange();
             }
