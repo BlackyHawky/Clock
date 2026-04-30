0aa722c2abb500f35c40fc074afb6d0dfbf8b27a
BlackyHawky <blackyhawky@protonmail.com>
2026-04-30 11:44:06 +0200

    Ability to keep the screen on for the screensaver launched from Settings

--- app/src/main/java/com/best/deskclock/settings/PreferencesKeys.java
+++ app/src/main/java/com/best/deskclock/settings/PreferencesKeys.java
@@ -221,6 +221,7 @@ public class PreferencesKeys {
     public static final String KEY_SCREENSAVER_DATE_IN_ITALIC = "key_screensaver_date_in_italic";
     public static final String KEY_SCREENSAVER_NEXT_ALARM_IN_BOLD = "key_screensaver_next_alarm_in_bold";
     public static final String KEY_SCREENSAVER_NEXT_ALARM_IN_ITALIC = "key_screensaver_next_alarm_in_italic";
+    public static final String KEY_SCREENSAVER_KEEP_SCREEN_ON = "key_screensaver_keep_screen_on";
     public static final String KEY_SCREENSAVER_BACKGROUND_IMAGE = "key_screensaver_background_image";
     public static final String KEY_ENABLE_SCREENSAVER_BLUR_EFFECT = "key_enable_screensaver_blur_effect";
     public static final String KEY_SCREENSAVER_BLUR_INTENSITY = "key_screensaver_blur_intensity";
